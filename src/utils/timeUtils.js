a72cd53326985520c10fc6c72f79edfd3f4cd26a
Mudit Sharma <94481502+MuditIsOP@users.noreply.github.com>
2026-01-01 14:02:20 +0530

    Update timeUtils.js

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -29,7 +29,7 @@ export const getCurrentHourIST = () => {
  */
 export const isMorningTime = () => {
     const hour = getCurrentHourIST();
-    return hour >= 5 && hour < 14;
+    return hour >= 5 && hour < 10;
 };
 
 /**
