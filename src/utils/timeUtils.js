b91756c3168defcb08400bdbd5c1d4bfad9475dc
Mudit Sharma <94481502+MuditIsOP@users.noreply.github.com>
2026-01-05 02:17:26 +0530

    Change USE_HARDCODED_TIME to true

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -8,7 +8,7 @@ const IST_ZONE = 'Asia/Kolkata';
 // Set USE_HARDCODED_TIME to true to use the date below
 // Set to false for live time
 // ============================================
-const USE_HARDCODED_TIME = false;
+const USE_HARDCODED_TIME = true;
 const HARDCODED_DATE = { year: 2026, month: 1, day: 4, hour: 23, minute: 55 };
 // ============================================
 
